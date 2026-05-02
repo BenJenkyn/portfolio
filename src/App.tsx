ed6b5f58876a73b797c351fdd1632d816a2579b7
BenJenkyn <jenkynb@gmail.com>
2026-05-02 14:56:19 -0400

    adds work experiences

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import styles from './app.module.css';
 import type { Component } from 'solid-js';
 import Navbar from './Navbar/Navbar';
 import GlassBox from './GlassBox/GlassBox';
-import { BiLogosTypescript, BiLogosHtml5, BiLogosCss3 } from 'solid-icons/bi'
-import { SiJquery } from 'solid-icons/si'
+import {
+  BiLogosTypescript,
+  BiLogosHtml5,
+  BiLogosCss3,
+  BiLogosJavascript,
+  BiLogosReact,
+  BiLogosNodejs,
+  BiLogosFirebase,
+  BiLogosJquery,
+} from 'solid-icons/bi'
+import { SiStrapi } from 'solid-icons/si'
 
 const App: Component = () => {
   return (
@@ -19,17 +28,32 @@ const App: Component = () => {
       </section>
       <section id="work_projects" class={`${styles.section} ${styles.workSection}`}>
         <h2 class={styles.sectionTitle}>Work Projects</h2>
-        <GlassBox
-          projectName='Lunch-a-Build Adventure'
-          projectDescription='The project has been taken down so please see this wayback machine link for reference. [MORE DESCRIPTION HERE]'
-          websiteLink='https://web.archive.org/web/20240812174437/https://www.lunchabuildadventure.com/'
-          techStack={[
-            { icon: <BiLogosTypescript size="32px" />, name: "TypeScript" },
-            { icon: <SiJquery size="32px" />, name: "jQuery" },
-            { icon: <BiLogosHtml5 size="32px" />, name: "HTML" },
-            { icon: <BiLogosCss3 size="32px" />, name: "CSS" },
-          ]}
-        />
+        <div class={styles.glass_box_section}>
+          <GlassBox
+            projectName='Lunch-a-Build Adventure'
+            projectDescription='The project has been taken down so please see this wayback machine link for reference. [MORE DESCRIPTION HERE]'
+            websiteLink='https://web.archive.org/web/20240812174437/https://www.lunchabuildadventure.com/'
+            techStack={[
+              { icon: <BiLogosTypescript size="32px" />, name: "TypeScript" },
+              { icon: <BiLogosJquery size="32px" />, name: "jQuery" },
+              { icon: <BiLogosHtml5 size="32px" />, name: "HTML" },
+              { icon: <BiLogosCss3 size="32px" />, name: "CSS" },
+              { icon: <BiLogosFirebase size="32px" />, name: "Firebase" },
+            ]}
+          />
+          <GlassBox
+            projectName='Zonetail Home'
+            projectDescription='For this project we experimented with using React Native Web for the explore page. [DESCRIPTION HERE]'
+            websiteLink='https://www.zonetailhome.com/'
+            techStack={[
+              { icon: <BiLogosTypescript size="32px" />, name: "TypeScript" },
+              { icon: <BiLogosReact size="32px" />, name: "React/React Native" },
+              { icon: <BiLogosNodejs size="32px" />, name: "NodeJS" },
+              { icon: <BiLogosJavascript size="32px" />, name: "JavaScript" },
+              { icon: <SiStrapi size="32px" />, name: "Strapi" },
+            ]}
+          />
+        </div>
       </section>
       <section id="school_projects" class={`${styles.section} ${styles.workSection}`}>
         <h2 class={styles.sectionTitle}>School Projects</h2>
